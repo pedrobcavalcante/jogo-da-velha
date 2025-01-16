62a8d66bb28b8852bfedd658fc85dc09d95cc7d1
pedrobcavalcante <pedrobcavalcante@users.noreply.github.com>
2025-01-16 21:49:55 +0000

    deploy: 06d3fe134c36eea03c6728124132d4a5c8944e6f

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: process.env.EXPORT_STATIC === "true" ? "export" : undefined,
-  basePath: "/jogo-da-velha",
-  trailingSlash: true,
-};
-
-export default nextConfig;
