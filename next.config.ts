16cd1587bfdec1ffeae00e737c9e8cf056719a6a
Pedro Benevides <pedrobenevides3243@gmail.com>
2025-01-16 18:50:55 -0300

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 const nextConfig = {
-  output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
-  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  output: "export",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
   trailingSlash: true,
   images: {
     unoptimized: true,
