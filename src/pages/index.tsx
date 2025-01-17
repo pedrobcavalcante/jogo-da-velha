fbd2ea492abb73011e283c6781203943c94e7b8a
Pedro Benevides <pedrobenevides3243@gmail.com>
2025-01-17 17:47:53 -0300

    last fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { GetStaticProps } from "next";
 import Board from "../components/Board";
-
+import { CSSProperties } from "react";
 type HomeProps = {
   initialSquares: (string | null)[];
 };
@@ -16,6 +16,7 @@ const Home: React.FC<HomeProps> = ({ initialSquares }) => {
   const [isGameOver, setIsGameOver] = useState<boolean>(false);
   const [winner, setWinner] = useState<string | null>(null);
   const [currentPlayer, setCurrentPlayer] = useState<string>("X");
+  const [showOverlay, setShowOverlay] = useState<boolean>(true);
 
   const resetGame = () => {
     setSquares(initialSquares);
@@ -23,6 +24,7 @@ const Home: React.FC<HomeProps> = ({ initialSquares }) => {
     setIsGameOver(false);
     setWinner(null);
     setCurrentPlayer("X");
+    setShowOverlay(true);
   };
 
   const checkWinner = (newSquares: (string | null)[]) => {
@@ -92,6 +94,7 @@ const Home: React.FC<HomeProps> = ({ initialSquares }) => {
   const handleGameModeChange = (mode: GameMode) => {
     setGameMode(mode);
     resetGame();
+    setShowOverlay(false);
   };
 
   const toggleDarkMode = () => {
@@ -114,34 +117,61 @@ const Home: React.FC<HomeProps> = ({ initialSquares }) => {
   }, [isDarkMode]);
 
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1 style={{ fontSize: "3rem", marginBottom: "30px" }}>Jogo da Velha</h1>
-
-      {isGameOver && (
-        <div style={{ marginBottom: "20px" }}>
-          <h2>{winner === "Empate" ? "Empate!" : `${winner} venceu!`}</h2>
-        </div>
-      )}
-
-      {!isGameOver && (
-        <div style={{ marginBottom: "20px" }}>
-          <button
-            onClick={() => handleGameModeChange("human-vs-human")}
-            style={buttonStyle}
+    <div
+      style={{
+        textAlign: "center",
+        position: "relative",
+        height: "100vh",
+        width: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {showOverlay && (
+        <div style={overlay}>
+          <h1
+            style={{ fontSize: "3rem", marginBottom: "30px", color: "black" }}
           >
-            Jogar contra outro jogador
-          </button>
-          <button
-            onClick={() => handleGameModeChange("human-vs-cpu")}
-            style={buttonStyle}
-          >
-            Jogar contra a CPU
-          </button>
+            Jogo da Velha
+          </h1>
+          {isGameOver && (
+            <div style={{ marginBottom: "20px" }}>
+              <h2>{winner === "Empate" ? "Empate!" : `${winner} venceu!`}</h2>
+            </div>
+          )}
+          {!isGameOver && (
+            <div style={{ marginBottom: "20px" }}>
+              <button
+                onClick={() => handleGameModeChange("human-vs-human")}
+                style={buttonStyle}
+              >
+                Jogar contra outro jogador
+              </button>
+              <button
+                onClick={() => handleGameModeChange("human-vs-cpu")}
+                style={buttonStyle}
+              >
+                Jogar contra a CPU
+              </button>
+            </div>
+          )}
+
+          <div style={{ marginTop: "20px" }}>
+            <button onClick={toggleDarkMode} style={buttonStyle}>
+              Modo {isDarkMode ? "Claro" : "Escuro"}
+            </button>
+          </div>
         </div>
       )}
 
       <div
-        style={{ marginTop: "30px", display: "flex", justifyContent: "center" }}
+        style={{
+          marginTop: "30px",
+          display: "flex",
+          justifyContent: "center",
+        }}
       >
         <Board initialSquares={squares} onSquareClick={handleSquareClick} />
       </div>
@@ -153,14 +183,23 @@ const Home: React.FC<HomeProps> = ({ initialSquares }) => {
           </button>
         </div>
       )}
-
-      <div style={{ marginTop: "20px" }}>
-        <button onClick={toggleDarkMode} style={buttonStyle}>
-          Alternar para Modo {isDarkMode ? "Claro" : "Escuro"}
-        </button>
-      </div>
     </div>
   );
+};
+
+const overlay: CSSProperties = {
+  position: "absolute",
+  top: "0",
+  left: "0",
+  width: "100%",
+  height: "100%",
+  backgroundColor: "rgba(255, 255, 255, 0.5)",
+  color: "white",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  backdropFilter: "blur(5px)",
 };
 
 const buttonStyle = {
